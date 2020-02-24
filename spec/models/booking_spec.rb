9ea991563aabbe5f44c0d4c01f05c12c9cae3631
NicholasZeitoun <nzeit8003@gmail.com>
2020-02-25 01:25:09 +1100

    completed models testing suite

---
+++ spec/models/booking_spec.rb
@@ -0,0 +1,117 @@
+require 'rails_helper'
+
+RSpec.describe "Booking", :type => :booking do
+  # Hash for valid user attributes to test build user
+  let(:valid_attributes) do
+    {
+      first_name: "Elton",
+      last_name: "John",
+      details: "An avid plant collector",
+      email: "elton.john@rocketman.com",
+      password: "password",
+      avatar_url: ""
+    }
+  end
+
+  let (:test_user) do
+    User.create!(valid_attributes)
+  end
+
+  # Hash for valid plant attributes to test user
+  let(:valid_plant) do
+    {
+      name: "Fern",
+      description: "A green fern",
+      location: "Richmond",
+      category: "green",
+      price: 42.69
+    }
+  end
+
+  let (:test_plant) do
+    user = User.create!(valid_attributes)
+    plant = Plant.new(valid_plant)
+    plant.user_id = user.id
+    plant.save!
+    puts plant
+  end
+  # Hash for booking plant attributes to test user
+  let(:valid_booking) do
+    {
+      start_date: DateTime.parse("25-2-2020"),
+      end_date: DateTime.parse("26-2-2020")
+    }
+  end
+
+  # Enforces these attributes when creating user: start_date, end_date
+
+  # Start
+  it "has a start_date" do
+    booking = Booking.new(start_date: DateTime.parse("25-2-2020"))
+    expect(booking.start_date).to eq(DateTime.parse("25-2-2020"))
+  end
+
+  it "start_date cannot be blank" do
+    attributes = valid_booking
+    attributes.delete(:start_date)
+    booking = Booking.new(attributes)
+    expect(booking).not_to be_valid
+  end
+
+  # End
+  it "has an end_date" do
+    booking = Booking.new(end_date: DateTime.parse("26-2-2020"))
+    expect(booking.end_date).to eq(DateTime.parse("26-2-2020"))
+  end
+
+  it "end_date cannot be blank" do
+    attributes = valid_attributes
+    attributes.delete(:last_name)
+    user = User.new(attributes)
+    expect(user).not_to be_valid
+  end
+
+  # Belongs to a plant
+  it "belongs to a plant" do
+    user = User.create!(valid_attributes)
+    plant = Plant.new(valid_plant)
+    plant.user_id = user.id
+    plant.save!
+    booking = Booking.new(valid_booking)
+    booking.user_id = user.id
+    booking.plant_id = plant.id
+    booking.save!
+    expect(booking.plant).to eq(plant)
+  end
+
+  # Belongs to a user
+  it "belongs to a user" do
+    user = User.create!(valid_attributes)
+    booking = Booking.new(valid_booking)
+    booking.user_id = user.id
+    expect(booking.user).to eq(user)
+  end
+
+  # Has many review children
+  it "has many reviews" do
+    booking = Booking.new(valid_booking)
+    expect(booking).to respond_to(:reviews)
+    expect(booking.reviews.count).to eq(0)
+  end
+
+  # Destroys children reviews when Booking is removed
+  it "should destroy child reviews when destroying self" do
+    user = User.create!(valid_attributes)
+    plant = Plant.new(valid_plant)
+    plant.user_id = user.id
+    plant.save!
+    booking = Booking.new(valid_booking)
+    booking.user_id = user.id
+    booking.plant_id = plant.id
+    booking.save!
+    review = Review.new(rating: 5)
+    review.booking_id = booking.id
+    review.save!
+    expect { booking.destroy }.to change { Review.count }.from(1).to(0)
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "User", :type => :user do
+  # Hash for valid user attributes to test build user
   let(:valid_attributes) do
     {
       first_name: "Elton",
@@ -11,7 +12,7 @@ RSpec.describe "User", :type => :user do
       avatar_url: ""
     }
   end
-
+  # Hash for valid plant attributes to test user
   let(:valid_plant) do
     {
       name: "Fern",
@@ -21,13 +22,27 @@ RSpec.describe "User", :type => :user do
       price: 42.69
     }
   end
+  # Hash for booking plant attributes to test user
+  let(:valid_booking) do
+    {
+      start_date: DateTime.parse("25-2-2020"),
+      end_date: DateTime.parse("26-2-2020")
+    }
+  end
 
-  # Enforces these attributes when creatign plant: name, location, price, category
+  # Enforces these attributes when creating user: first name, last_name
 
   # name
-  it "has a first name" do
+  it "has a first_name" do
     user = User.new(first_name: "Elton")
     expect(user.first_name).to eq("Elton")
+  end
+
+  it "first_name cannot be blank" do
+    attributes = valid_attributes
+    attributes.delete(:first_name)
+    user = User.new(attributes)
+    expect(user).not_to be_valid
   end
 
   it "has a last name" do
@@ -35,6 +50,14 @@ RSpec.describe "User", :type => :user do
     expect(user.last_name).to eq("John")
   end
 
+  it "last_name cannot be blank" do
+    attributes = valid_attributes
+    attributes.delete(:last_name)
+    user = User.new(attributes)
+    expect(user).not_to be_valid
+  end
+
+  # Destroys children plants and bookings when User is removed
   it "should destroy child plants when destroying self" do
     user = User.create!(valid_attributes)
     plant = Plant.new(valid_plant)
@@ -43,47 +66,15 @@ RSpec.describe "User", :type => :user do
     expect { user.destroy }.to change { Plant.count }.from(1).to(0)
   end
 
-  # it "name cannot be blank" do
-  #   attributes = valid_attributes
-  #   attributes.delete(:name)
-  #   plant = Plant.new(attributes)
-  #   expect(plant).not_to be_valid
-  # end
-
-  # # location
-  # it "has a location" do
-  #   plant = Plant.new(location: "Richmond")
-  #   expect(plant.location).to eq("Richmond")
-  # end
-
-  # it "location cannot be blank" do
-  #   attributes = valid_attributes
-  #   attributes.delete(:location)
-  #   plant = Plant.new(attributes)
-  #   expect(plant).not_to be_valid
-  # end
-
-  # # price
-  # it "has a price" do
-  #   plant = Plant.new(price: 42.69)
-  #   expect(plant.price).to eq(42.69)
-  # end
-
-  # it "price is a float" do
-  #   plant = Plant.new(price: "invalid")
-  #   expect(plant).not_to be_valid
-  # end
-
-  # it "price cannot be blank" do
-  #   attributes = valid_attributes
-  #   attributes.delete(:price)
-  #   plant = Plant.new(attributes)
-  #   expect(plant).not_to be_valid
-  # end
-
-  # it "has many bookings" do
-  #   plant = Plant.new(valid_attributes)
-  #   expect(plant).to respond_to(:bookings)
-  #   expect(plant.bookings.count).to eq(0)
-  # end
+  it "should destroy child plant booking when destroying self" do
+    user = User.create!(valid_attributes)
+    plant = Plant.new(valid_plant)
+    plant.user_id = user.id
+    plant.save!
+    booking = Booking.new(valid_booking)
+    booking.user_id = user.id
+    booking.plant_id = plant.id
+    booking.save!
+    expect { user.destroy }.to change { Booking.count }.from(1).to(0)
+  end
 end
