ec0d169a6b99e0100cdf7b248bb3fbbcbf1ce91f
Juliette Ferrer <juliette.ferrerpro@gmail.com>
2020-02-25 17:29:31 +1100

    quick root fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
-  root to: 'pages#home'
+  #root to: 'pages#home'
+  root to: 'plants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
   resources :reviews, only: [:destroy]
