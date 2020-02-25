2bf24a43db0633737f421d7e59442f1fdb781c92
Juliette Ferrer <juliette.ferrerpro@gmail.com>
2020-02-25 13:52:07 +1100

    main methods and views set up for testing, lacks set_total_price

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,50 @@
+class BookingsController < ApplicationController
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.plant_id = params[:plant_id]
+    @booking.total_price = set_total_price
+    @booking.status = 'Pending'
+
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    # /!\ we should only be able to edit status
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    # /!\ we should only be able to edit status
+    if @booking.update(booking_params)
+      redirect_to booking_path(@booking)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :status)
+  end
+
+  def set_total_price
+    @plant = Plant.find(params[:plant_id])
+    # Need to retrieve start date and end date from params (query maybe?)
+    (@booking.start_date - @booking.end_date) * @plant.price
+  end
+end
+
