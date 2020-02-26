9d885eb4b60b994f410551ff0bef53752017f641
NicholasZeitoun <nzeit8003@gmail.com>
2020-02-26 12:27:53 +1100

    Changed the functionality of booking status

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,10 @@
 class Booking < ApplicationRecord
+  STATUSES = %w(Pending Approved Cancelled Completed)
   belongs_to :plant
   belongs_to :user
   has_one :review, dependent: :destroy
 
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validates :status, inclusion: { in: STATUSES }
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -38,6 +38,7 @@ RSpec.describe "Booking", :type => :booking do
   # Hash for booking plant attributes to test user
   let(:valid_booking) do
     {
+      status: 'Pending',
       start_date: DateTime.parse("25-2-2020"),
       end_date: DateTime.parse("26-2-2020")
     }
